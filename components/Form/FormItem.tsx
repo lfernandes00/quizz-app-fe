ea10983dac3e19e7f647440c421b219000bb8ecf
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-03-29 23:23:59 +0100

    Create form Item component

---
+++ components/Form/FormItem.tsx
@@ -0,0 +1,77 @@
+import { Form, Input } from "antd";
+
+import styles from "./FormItem.module.scss";
+
+interface IProps {
+  name?: string;
+  label?: string;
+  placeholder: string;
+  required?: boolean;
+  type?: "email" | "number";
+  message?: string;
+  isPassword?: boolean;
+  isTextArea?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+}
+
+export default function FormItem(props: IProps) {
+  const { name, label, placeholder, required, type, message, isPassword, isTextArea, onChange } =
+    props;
+
+  return (
+    <div className={styles.formItem}>
+      {!isPassword && !isTextArea && (
+        <Form.Item
+          name={[name]}
+          label={label ? label : undefined}
+          validateTrigger="onSubmit"
+          rules={[
+            {
+              required,
+              type,
+              message
+            }
+          ]}>
+          <Input placeholder={placeholder} className={styles.input} onChange={onChange} />
+        </Form.Item>
+      )}
+
+      {isPassword && (
+        <Form.Item
+          name={[name]}
+          label={label ? label : undefined}
+          validateTrigger="onSubmit"
+          rules={[
+            {
+              required,
+              type,
+              message
+            }
+          ]}>
+          <Input.Password placeholder={placeholder} className={styles.input} />
+        </Form.Item>
+      )}
+
+      {isTextArea && (
+        <Form.Item
+          name={[name]}
+          label={label ? label : undefined}
+          validateTrigger="onSubmit"
+          rules={[
+            {
+              required,
+              type,
+              message
+            }
+          ]}>
+          <Input.TextArea
+            autoSize={{ minRows: 5 }}
+            placeholder={placeholder}
+            className={styles.input}
+            onChange={onChange}
+          />
+        </Form.Item>
+      )}
+    </div>
+  );
+}
