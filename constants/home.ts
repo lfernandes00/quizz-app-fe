154d0e0e78f7d0b8856538a42992926acc37e3a9
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-04-01 00:25:37 +0100

    change images for testing

--- constants/home.ts
+++ constants/home.ts
@@ -18,15 +18,16 @@ export const posts: IPost[] = [
     id: 1,
     name: "Mário Freitas",
     username: "mfreitas98",
-    description: `Hey quiz enthusiasts! We've just added a new category of quizzes to our app - "80s Movie Trivia"! Test your knowledge of classic movies like The Breakfast Club, Ferris Bueller's Day Off, and E.T. the Extra-Terrestrial.`,
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/80-movies-1570826605.png?crop=0.984xw:0.984xh;0,0&resize=640:*"
+    description: `Hey quiz enthusiasts! We've just added a new category of quizzes to our app - "80s Movie Trivia"! Test your knowledge of classic movies like The Breakfast Club, Ferris Bueller's Day Off, and E.T. the Extra-Terrestrial.`
+    // image:
+    //   "https://hips.hearstapps.com/hmg-prod/images/80-movies-1570826605.png?crop=0.984xw:0.984xh;0,0&resize=640:*"
   },
   {
     id: 2,
     name: "Pedro Ferreira",
     username: "pedro.sferreira",
-    description: `Are you a history buff? Put your knowledge to the test with our "World History Challenge" quiz!`,
-    image: "https://cdn.britannica.com/26/188426-050-2AF26954/Germany-Poland-September-1-1939.jpg"
+    image: "/avatar.png",
+    description: `Are you a history buff? Put your knowledge to the test with our "World History Challenge" quiz!`
+    // image: "https://cdn.britannica.com/26/188426-050-2AF26954/Germany-Poland-September-1-1939.jpg"
   }
 ];
